0ee5b56c414881618a8a7405539a80db552a0628
hcharp <ln_de_troie@hotmail.fr>
2021-07-10 16:53:31 +0200

    cannot do this yet - draft

---
+++ CLIOutils.java
@@ -0,0 +1,16 @@
+public class CLIOutils {
+
+    String descriptionAlpha;
+    String descriptionEpsilon;
+    String descriptionIndice;
+    String descriptionMode;
+    
+    public static fromClass(Configuration myConf) {
+
+        CLIClassique myCLI;
+        
+        
+
+        return myCLI;
+    }
+}
